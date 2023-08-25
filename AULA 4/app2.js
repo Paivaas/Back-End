a13a7c4aecc8609279636a21c6e323d3a6a940fa
Julia Paiva <juliapoliver12@gmail.com>
2023-08-25 10:10:38 -0300

    Back End - JavaScript

--- AULA 4/app2.js
+++ AULA 4/app2.js
@@ -69,10 +69,11 @@ entradaDeDados.question('Digite o primeiro valor: ', function(numero1){
                 } else
                 //      console.log('ERRO: É obrigatório informar uma operação válida') */
 
+                
 
                 //Utilizando Switch Case - Versão 2.0
 
-                switch(operacao){
+                /* switch(operacao){
                     case 'SOMAR':
                     case '+':
                         resultado = valor1 + valor2;
@@ -95,11 +96,11 @@ entradaDeDados.question('Digite o primeiro valor: ', function(numero1){
                     default: console.log('ERRO: É obrigatório informar uma operação válida')
                     break;
 
-                }
+                }*/
+                resultado = calculadora(valor1, valor2, operacao)
 
                 //Validadação da variavél undefined
-                if (resultado != undefined){
-
+                if (resultado){
                 console.log(resultado)
                 }
                 entradaDeDados.close()
@@ -119,7 +120,7 @@ function calculadora(numero1, numero2, tipoCalculo){
     //Recebendo os valores dos argumentos da funçao em variaveis locais
     let valor1 = numero1
     let valor2 = numero2
-    let operacao = tipoCalculolo
+    let operacao = tipoCalculo
     let resultado;
 
     switch(operacao){
@@ -147,6 +148,12 @@ function calculadora(numero1, numero2, tipoCalculo){
 
     }
 
-    return
+    if (resultado != undefined)
+        if(String(resultado).indexOf('.')!=-1)
+            return Number(resultado).toFixed(2)
+        else
+            return resultado
+    else
+        return false
 
 }
