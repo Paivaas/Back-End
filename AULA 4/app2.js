20bdd4c183103eb46e877765b2592f46bdb7bd17
Julia Paiva <juliapoliver12@gmail.com>
2023-08-25 09:32:21 -0300

    Back End - JavaScript

--- AULA 4/app2.js
+++ AULA 4/app2.js
@@ -113,6 +113,39 @@ entradaDeDados.question('Digite o primeiro valor: ', function(numero1){
 })
 
 // Modelo tradicional de criar funções
-function calculadora(){
+// () o que estiver dentro se chama argumento
+function calculadora(numero1, numero2, tipoCalculo){
+
+    //Recebendo os valores dos argumentos da funçao em variaveis locais
+    let valor1 = numero1
+    let valor2 = numero2
+    let operacao = tipoCalculo
+
+    switch(operacao){
+        case 'SOMAR':
+        case '+':
+            resultado = valor1 + valor2;
+            break;
+        case 'SUBTRAIR':
+        case '-':
+            resultado = valor1 - valor2;
+            break;
+        case 'MULTIPLICAR':
+        case '*':
+            resultado = valor1 * valor2;
+            break;
+        case 'DIVIDIR':
+        case '/':
+            if (valor2 != 0)
+            resultado = valor1 / valor2;
+            else 
+            console.log('ERRO? Não é possível realizar uma divisão por 0')
+        break;
+        default: console.log('ERRO: É obrigatório informar uma operação válida')
+        break;
+
+    }
+
+    return
 
 }
