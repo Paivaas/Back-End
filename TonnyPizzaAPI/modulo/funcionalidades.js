68be41cd434ff6c02729c86d18165e6e8635b9cd
Julia Paiva <juliapoliver12@gmail.com>
2023-12-13 01:06:05 -0300

    Back End

--- TonnyPizzaAPI/modulo/funcionalidades.js
+++ TonnyPizzaAPI/modulo/funcionalidades.js
@@ -1,3 +1,116 @@
-var dados = require('./dados')
+var dados = require('../dados')
+
+// Funcionando
+const getListaUsuarios = () => {
+
+    const info = dados.usuarios.dados 
+    let usuariosA = []
+    let usuariosJ = {} 
+    
+    info.forEach((dados) =>{
+       
+        let dadosFinais = {
+            id: dados.nome
+        }
+        
+        usuariosA.push(dadosFinais)
+
+    })
+
+    usuariosJ.dados = usuariosA
+    usuariosJ.usuarios = info.length
+
+   console.log(usuariosJ)
+
+}
+getListaUsuarios()
+
+//Funcionando
+const getListaPizza = () => {
+
+    const info = dados.produtos.pizzas
+    let produtosA = []
+    let produtosJ = {}
+
+    info.forEach((dados) => {
+
+        let dadosFinais = {
+            id: dados.id,
+            categoria: dados.categoria,
+            nome: dados.nome,
+            descricao: dados.descricao,
+            preco: dados.preco
+        }
+
+        produtosA.push(dadosFinais)
+
+    })
+
+    produtosJ.dados = produtosA
+    produtosJ.quantidade = info.length
+
+    console.log(produtosJ)
+
+}
+getListaPizza()
+
+// Funcionando
+const getListaBebidas = () => {
+
+    const info = dados.produtos.bebidas
+    let produtosA = []
+    let produtosJ = {}
+
+    info.forEach((dados) => {
+
+        let dadosFinais = {
+            id: dados.id,
+            categoria: dados.categoria,
+            nome: dados.nome,
+            descricao: dados.descricao,
+            preco: dados.preco
+        }
+
+        produtosA.push(dadosFinais)
+
+    })
+
+    produtosJ.dados = produtosA
+    produtosJ.quantidade = info.length
+
+    console.log(produtosJ)
+
+}
+getListaBebidas()
+
+const getListarCategorias = () => {
+
+    const categoriaInfo = pizzaria.categorias.categoria
+    let categoriaArray = []
+    let categoriaJSON = {}
+
+    categoriaInfo.forEach((categoria) => {
+
+        let categoriasJSON = {
+            id: categoria.id,
+            nome_categoria: categoria.nome_categoria
+        }
+
+        categoriaArray.push(categoriasJSON)
+
+    })
+
+    categoriaJSON.categoria = categoriaArray
+    categoriaJSON.quantidade = categoriaInfo.length
+
+    return categoriaJSON
+
+}
 
 
+
+module.exports = {
+    getListaUsuarios,
+    getListaBebidas,
+    getListaPizza
+}
